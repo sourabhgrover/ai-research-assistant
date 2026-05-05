052edf54ccbe5b203ffa83733a141c614d40707f
sourabhgrover <sourabhgrover29@gmail.com>
2026-05-05 14:32:20 +0530

    feat: implement interactive agent loop with LangChain and Groq LLM integration

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from ai-research-assistant!")
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain.tools import tool
+import os
+
+# Load .env
+load_dotenv()
+
+# LLM
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+# Tools
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    return a * b
+
+#Agent
+agent = create_agent(model=llm,tools=[multiply])
 
 
-if __name__ == "__main__":
-    main()
+while True:
+    query = input("User: ")
+
+    if query.lower() in ["exit","quit"]:
+        break
+
+    response = agent.invoke({
+        "messages":[{"role":"user","content":query}]
+    })
+    print("Agent: ",response["messages"][-1].content)
